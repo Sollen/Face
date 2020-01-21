28240809275a22d5c26f09d50ef9f2bfb4975b6d
Ilya Fedosov <ilya.fedosov@antidasoftware.com>
2020-01-21 15:19:18 +0500

    Add Accord service Refactoring

---
+++ Services/FindfaceAccord.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Accord.Vision.Detection;
+using Accord.Vision.Detection.Cascades;
+
+namespace Face.Services
+{
+    public class FindfaceAccord : FindfaceBase
+    {
+        protected override IEnumerable<Rectangle> GetFaces(Bitmap bitmap)
+        {
+            var faceDetector = new HaarObjectDetector(new FaceHaarCascade(), minSize: 25, searchMode: ObjectDetectorSearchMode.NoOverlap);
+
+            return faceDetector.ProcessFrame(bitmap);
+        }
+    }
+}
---
+++ Services/FindfaceBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Face.Services
+{
+    public abstract class FindfaceBase : IFindfaceService
+    {
+        public Image FindAndDraw(string path)
+        {
+            var img = Image.FromFile(path);
+            var bitmap = new Bitmap(img);
+
+            var faces = GetFaces(bitmap);
+
+            foreach (var face in faces)
+                using (var graphics = Graphics.FromImage(bitmap))
+                using (var pen = new Pen(Color.Yellow, 3))
+                    graphics.DrawRectangle(pen, face);
+
+            return bitmap;
+
+        }
+
+        protected abstract IEnumerable<Rectangle> GetFaces(Bitmap bitmap);
+
+    }
+}
